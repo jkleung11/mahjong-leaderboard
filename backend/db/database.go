582d16aeb5817680fde0932357defec1a2714212
jkleung11 <leungjk11+github@gmail.com>
2025-02-20 15:16:04 -0500

    foreign key constraints

--- backend/db/database.go
+++ backend/db/database.go
@@ -10,7 +10,16 @@ import (
 func ConnectDB(dbPath string) *gorm.DB {
 	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
 	if err != nil {
-		log.Fatalf("Failed to connnect to the database: %v", err)
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("Failed to get database connection: %v", err)
+	}
+	_, err = sqlDB.Exec("PRAGMA foreign_keys=ON;")
+	if err != nil {
+		log.Fatalf("Failed to enable foreign key constraints: %v", err)
 	}
 
 	return db
--- backend/models/game.go
+++ backend/models/game.go
@@ -8,7 +8,8 @@ import (
 
 type Game struct {
 	gorm.Model
-	Date          time.Time
-	WinnerID      *uint `gorm:"index;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignKey:WinnerID;references:ID" json:"winner_id"`
-	WinningPoints *uint `json:"winning_points"`
+	Date          time.Time `json:"date"`
+	WinnerID      *uint     `gorm:"index;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignKey:WinnerID;references:ID" json:"winner_id"`
+	Winner        *Player   `gorm:"foreignKey:WinnerID;references:ID"`
+	WinningPoints *uint     `json:"winning_points"`
 }
