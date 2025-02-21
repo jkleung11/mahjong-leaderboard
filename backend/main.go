bbfe042f5f5f12849335424b0d88218ec263ff3d
jkleung11 <leungjk11+github@gmail.com>
2025-02-21 15:33:25 -0500

    model and migration

--- backend/main.go
+++ backend/main.go
@@ -38,5 +38,10 @@ func main() {
 
 // RunMigrations applies database migrations
 func RunMigrations(db *gorm.DB) error {
-	return db.AutoMigrate(&models.Player{}, &models.Game{})
+	modelsToMigrate := []interface{}{
+		&models.Player{},
+		&models.Game{},
+		&models.GamePlayers{},
+	}
+	return db.AutoMigrate(modelsToMigrate...)
 }
