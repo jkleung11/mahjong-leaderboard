3260a228bd644ec4e79b3dd8d5b35804ec90b320
jkleung11 <leungjk11+github@gmail.com>
2025-02-21 16:51:15 -0500

    wip, need to clean up bindings

--- backend/handlers/game.go
+++ backend/handlers/game.go
@@ -1,9 +1,16 @@
+/*
+Games from the front end will include all of our information (who played, who won).
+This handler should validate that format includes what we need to create
+entries for our Game and GamePlayers records.
+*/
+
 package handlers
 
 import (
 	"fmt"
 	"mahjong-leaderboard-backend/models"
 	"net/http"
+	"slices"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -16,28 +23,50 @@ type GameHandler struct {
 }
 
 type GameRequest struct {
+	// Information provided from user on
+	Date          string   `json:"date" binding:"required"`
+	Winner        *string  `json:"winner" binding:"required"`
+	WinningPoints *uint    `json:"winning_points" binding:"required"`
+	Players       []string `json:"players" binding:"required"`
+}
+
+type GamePayload struct {
+	// The actual payload for the record in the games table
 	Date          string `json:"date" binding:"required"`
 	WinnerID      *uint  `json:"winner_id" binding:"required"`
 	WinningPoints *uint  `json:"winning_points" binding:"required"`
 }
 
+func ValidateGameRequest(request GameRequest) error {
+	if len(request.Players) != 4 {
+		return fmt.Errorf("A game must have exactly 4 players")
+	}
+	if len(request.Winner) > 0 {
+		if slices.Contains(request.Players, &request.Winner) {
+			return nil
+		}
+	}
+
+	return nil
+}
+
 // create a game
 func (h *GameHandler) CreateGame(c *gin.Context) {
-	var request GameRequest
-	if err := c.ShouldBindJSON(&request); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "missing required fields in request"})
+	var payload GamePayload
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "missing required fields in payload"})
 		return
 	}
 
-	parsedDate, err := time.Parse(time.RFC3339, request.Date)
+	parsedDate, err := time.Parse(time.RFC3339, payload.Date)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid date format, need RFC3339"})
 		return
 	}
 	game := models.Game{
 		Date:          parsedDate,
-		WinnerID:      request.WinnerID,
-		WinningPoints: request.WinningPoints,
+		WinnerID:      payload.WinnerID,
+		WinningPoints: payload.WinningPoints,
 	}
 
 	if err := h.DB.Create(&game).Error; err != nil {
